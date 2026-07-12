c7215cb9d343692b3e34d15eefda02e851ddf788
bodane <49066403+bodane@users.noreply.github.com>
2026-07-13 02:39:19 +1000

    connect mcp client to server + use local llm via ollama

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+#from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain.agents import create_agent
 from mcp import ClientSession, StdioServerParameters
@@ -10,7 +12,8 @@ from mcp.client.stdio import stdio_client
 
 load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatOllama(model="hf.co/lthn/lemma:Q4_K_M") 
+#llm = ChatOpenAI()
 
 stdio_server_params = StdioServerParameters(
     command="python",
@@ -18,8 +21,17 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
 
+            
+            agent = create_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="Using the math tool, what is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
