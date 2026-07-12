1d071603326e853375fe00104a9d07c73fb4850f
bodane <49066403+bodane@users.noreply.github.com>
2026-07-13 00:16:42 +1000

    add imports, deps, llm and stdio code

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args={r"E:\Training\MCP Crash Course - Complete Model Context Protocol in a Day\Section_6\39\mcp-crash-course\servers\math_server.py"},
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
